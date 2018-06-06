e938e28c343639151e6efa405f0fa5f18e646391
Erik Nordland <eriknordland@gmail,com>
2018-06-05 18:16:56 -0700

    puzzling done

---
+++ puzzling.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+public class puzzling {
+    public ArrayList<Object> greaterThan(int [] arr, int cutoff) {
+        ArrayList<Object> newarr = new ArrayList<Object>();
+        boolean notempty = false; 
+        for(int i = 0;i < arr.length;i++){
+            if(arr[i]>cutoff){
+                notempty = true;
+                newarr.add(arr[i]);
+            }
+        }
+        return newarr;
+    }
+    public ArrayList<Object>  shufflePrint(String [] str_list, int cutoff){
+        ArrayList<Object> newarr = new ArrayList<Object>();
+        ArrayList<String> shuffle = new ArrayList<String>();
+        int string_length;
+        shuffle.addAll(Arrays.asList(str_list));
+        Collections.shuffle(shuffle);
+        for(String this_string: shuffle){
+            System.out.print(this_string);
+            System.out.print(" ");
+            string_length = this_string.length();
+            if(string_length>cutoff){
+                newarr.add(this_string);
+            }
+        }
+        return newarr;
+    }
+    public char [] alphaShufflePrint(String char_list){
+        char [] newarr = new char[2]; //first and last letters
+        ArrayList<Character> shuffle = new ArrayList<Character>();
+        for (char c : char_list.toCharArray()) {
+            shuffle.add(c);
+        }
+        Collections.shuffle(shuffle);
+        newarr[0] = shuffle.get(0);
+        if ((newarr[0] == 'a') || (newarr[0] == 'e') || (newarr[0] == 'i') || (newarr[0] == 'o') || (newarr[0] == 'u')){
+            System.out.println("You caught a vowel!");
+        }
+        newarr[1] = shuffle.get(shuffle.size()-1);
+        return newarr;
+    }
+    public int [] randomInRange(int arr_size, int lower, int upper){
+        int [] output = new int[arr_size];
+        ArrayList<Integer> sort = new ArrayList<Integer>();
+        output[0] = ThreadLocalRandom.current().nextInt(lower,(upper + 1));
+        sort.add(output[0]);
+        int min = output[0];
+        int max = output[0];
+        //System.out.println("Array:");
+        //System.out.print(output[0]);
+        for(int i = 1; i < arr_size; i++){
+            output[i] = ThreadLocalRandom.current().nextInt(lower,(upper + 1));
+            if (output[i]<min){
+                min = output[i];
+            }
+            if (output[i]>max){
+                max = output[i];
+            }
+            sort.add(output[i]);
+            // System.out.print(" ");
+            // System.out.print(output[i]);
+        }
+        // System.out.println(sort);
+        // System.out.println("Sorted: ");
+        // Collections.sort(sort);
+        // System.out.println(sort);
+        // System.out.println("");
+        // System.out.print("Minimum: ");
+        // System.out.print(min);
+        // System.out.println("");
+        // System.out.print("Maximum: ");
+        // System.out.print(max);
+        return output;
+    }
+    public String randWords(int word_size){
+        String newWord = "";
+        int [] arr2 = this.randomInRange(word_size,0,26);
+        String alpha = "abcdedfghijklmnopqrstuvwxyz";
+        for (int i = 0; i < arr2.length; i++){
+           newWord += Character.toString(alpha.charAt(arr2[i]));
+        }
+        return newWord;
+    }
+    public String [] randWords(int word_size, int numWords){
+        String this_word;
+        String [] output = new String[numWords];
+        for(int i = 0; i < numWords; i++){
+            output[i] = randWords(word_size);
+        }
+        return output;
+    }
+
+}
--- run.java
+++ run.java
@@ -1,38 +1,101 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 public class run {
     public static void main(String[] args) {
-        //Pythagorean
-        pythagorean p = new pythagorean();
-        int A = 3;
-        int B = 4;
-        double hypotenuse = p.calculateHypotenuse(A,B);
-        // System.out.println("Your A is: " + A);
-        // System.out.println("Your B is: " + B);
-        // System.out.println("Your hypotenuse is:" + hypotenuse);
+        // //Pythagorean
+        // pythagorean p = new pythagorean();
+        // int A = 3;
+        // int B = 4;
+        // double hypotenuse = p.calculateHypotenuse(A,B);
+        // // System.out.println("Your A is: " + A);
+        // // System.out.println("Your B is: " + B);
+        // // System.out.println("Your hypotenuse is:" + hypotenuse);
 
-        //FizzBuzz
-        Fizzbuzz fizz = new Fizzbuzz();
-        String one = fizz.fizzBuzz(3);
-        String two = fizz.fizzBuzz(5);
-        String three = fizz.fizzBuzz(15);
-        String four = fizz.fizzBuzz(2);
-        //System.out.println("Fizzbuzz: " + one + " " + two + " " + three + four);
+        // //FizzBuzz
+        // Fizzbuzz fizz = new Fizzbuzz();
+        // String one = fizz.fizzBuzz(3);
+        // String two = fizz.fizzBuzz(5);
+        // String three = fizz.fizzBuzz(15);
+        // String four = fizz.fizzBuzz(2);
+        // //System.out.println("Fizzbuzz: " + one + " " + two + " " + three + four);
 
-        //String Manipulator
-        StringManipulator mani = new StringManipulator();
-        String str = mani.trimAndConcat("      Test ","  Me    ");
-        Integer index1 = mani.getIndexOrNull(str,'t');
-        Integer index2 = mani.getIndexOrNull(str,'M');
-        Integer index3 = mani.getIndexOrNull(str,'z');
-        Integer ind1 = mani.getIndexOrNull(str, "est");
-        String word = mani.concatSubstring("Hello",1,2,"world");
-        Integer ind2 = mani.getIndexOrNull(str, "tMx");
-        System.out.println("expected: 3 4 Null 1 Null");
-        System.out.print(str + " ");
-        System.out.print(index1);
-        System.out.print(index2);
-        System.out.print(index3);
-        System.out.print(ind1);
-        System.out.print(ind2);
-        System.out.println(word);
+        // //String Manipulator
+        // StringManipulator mani = new StringManipulator();
+        // String str = mani.trimAndConcat("      Test ","  Me    ");
+        // Integer index1 = mani.getIndexOrNull(str,'t');
+        // Integer index2 = mani.getIndexOrNull(str,'M');
+        // Integer index3 = mani.getIndexOrNull(str,'z');
+        // Integer ind1 = mani.getIndexOrNull(str, "est");
+        // String word = mani.concatSubstring("Hello",1,2,"world");
+        // Integer ind2 = mani.getIndexOrNull(str, "tMx");
+        // System.out.println("expected: 3 4 Null 1 Null");
+        // System.out.print(str + " ");
+        // System.out.print(index1);
+        // System.out.print(index2);
+        // System.out.print(index3);
+        // System.out.print(ind1);
+        // System.out.print(ind2);
+        // System.out.println(word);
+
+        //Puzzling
+        //1
+        puzzling puzzle = new puzzling();
+        int [] list = {3,5,1,2,7,9,8,13,25,32};
+        ArrayList<Object> test = new ArrayList<Object>();
+        test = puzzle.greaterThan(list,10);
+        System.out.println("------------Puzzle 1------------");
+        System.out.println("Old List:");
+        for(int i = 0; i < list.length; i++){
+            System.out.print(list[i]);
+            System.out.print(" ");
+        }
+        System.out.println("");
+        System.out.println("New List:");
+        System.out.println(test);
+        test.clear();
+        //2
+        System.out.println("------------Puzzle 2------------");
+        String [] strlist = new String []{"Nancy","Jinichi","Fujibayashi","Momochi","Ishikawa"};
+        System.out.println("Original Array: ");
+        for(int i = 0; i < strlist.length; i++){
+            System.out.print(strlist[i]);
+            System.out.print(" ");
+        }
+        System.out.println("");
+        System.out.println("Shuffled Array: ");
+        int cutoff = 5;
+        test = puzzle.shufflePrint(strlist,cutoff);
+        System.out.println("");
+        System.out.print("Longer than ");
+        System.out.print(cutoff);
+        System.out.print(" characters: ");
+        System.out.println("");
+        System.out.println(test);
+        //3
+        System.out.println("------------Puzzle 3------------");
+        String alpha = "abcdedfghijklmnopqrstuvwxyz";
+        System.out.println("Your first and last characters are: ");
+        char [] first_last = puzzle.alphaShufflePrint(alpha);
+        System.out.println(first_last);
+        //4
+        System.out.println("------------Puzzle 4------------");
+        int [] arr1 = puzzle.randomInRange(10,55,100);
+        System.out.println(""); //uncomment system prints to see output
+        //5
+        System.out.println("------------Puzzle 5------------");
+        String randomWord = puzzle.randWords(5);
+        System.out.println(randomWord);
+        //6
+        System.out.println("------------Puzzle 6------------");
+        String [] words = puzzle.randWords(5,10);
+        System.out.println("");
+        System.out.println("Random Words: ");
+        System.out.println("");
+        for(int i = 0; i < words.length; i++){
+            System.out.print(words[i]);
+            System.out.print(" ");
+        }
     }
 }
